d5d214dd30311256abacb18fe180b9204d9dcc4e
Şafak Bilici <44443472+safakkbilici@users.noreply.github.com>
2020-06-30 16:16:04 +0300

    Add files via upload

---
+++ 2x3/BLM2021 - x86 ASM/Homework1 Source Codes/main.cpp
@@ -0,0 +1,63 @@
+#include <windows.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <iostream>
+#include "image_processing.cpp"
+
+using namespace std;
+
+void SteganografiBul(int n, int resimadres_org, int resimadres_ste, int steganografi_adres);
+
+int main(void) {
+	int M, N, Q, i, j;
+	bool type;
+	int efile;
+	char org_resim[100], ste_resim[100], steganografi[100];
+	do {
+		printf("Orijinal resmin yolunu (path) giriniz:\n-> ");
+		scanf("%s", &org_resim);
+		system("CLS");
+		efile = readImageHeader(org_resim, N, M, Q, type);
+	} while (efile > 1);
+	int** resim_org = resimOku(org_resim);
+
+	do {
+		printf("Steganografik resmin yolunu (path) giriniz:\n-> ");
+		scanf("%s", &ste_resim);
+		system("CLS");
+		efile = readImageHeader(ste_resim, N, M, Q, type);
+	} while (efile > 1);
+	int** resim_ste = resimOku(ste_resim);
+
+	printf("Orjinal Resim Yolu: \t\t\t%s\n", org_resim);
+	printf("SteganografiK Resim Yolu: \t\t%s\n", ste_resim);
+
+	short *resimdizi_org, *resimdizi_ste;
+	resimdizi_org = (short*) malloc(N*M * sizeof(short));
+	resimdizi_ste = (short*) malloc(N*M * sizeof(short));
+
+	for (i = 0; i < N; i++) 
+		for (j = 0; j < M; j++) {
+			resimdizi_org[i*N + j] = (short) resim_org[i][j];
+			resimdizi_ste[i*N + j] = (short) resim_ste[i][j];
+		}
+
+	int resimadres_org = (int) resimdizi_org;
+	int resimadres_ste = (int) resimdizi_ste;
+	int steganografi_adres = (int) steganografi;
+
+	SteganografiBul(N*M, resimadres_org, resimadres_ste, steganografi_adres);
+
+	printf("\nResim icerisinde gizlenmis kod: \t%s\n", steganografi);
+	system("PAUSE");
+	return 0;
+}
+
+void SteganografiBul(int n, int resim_org, int resim_ste, int steganografi_adres) {
+	__asm {
+		//KODUNUZU BURAYA YAZINIZ, ASAGIDAKI 2 SATIRI SILMELISINIZ
+		MOV EBX, steganografi_adres
+		MOV BYTE PTR [EBX], 0
+	}
+}
